20008a0d8bcd43e1fa2c51efb7e0ae767efb1ce4
JosueGC369 <jguerreroc2400@alumno.ipn.mx>
2026-09-19 18:46:30 -0600

    cambios en style

---
+++ Primer Parcial/02Ejercicios/app.js
@@ -0,0 +1,28 @@
+const talleres = [
+  { nombre: 'Introducción a Python', instructor: 'Ing. María López', cupo: 25, inscritos: 25 },
+  { nombre: 'Fundamentos de Redes', instructor: 'Ing. Carlos Ramírez', cupo: 30, inscritos: 18 },
+  { nombre: 'Diseño de Bases de Datos', instructor: 'Ing. Ana Torres', cupo: 20, inscritos: 20 },
+  { nombre: 'Desarrollo Web con JS', instructor: 'Ing. María López', cupo: 25, inscritos: 10 },
+];
+
+function pintarTabla(){
+
+}
+//debe de obtener la tabla y llenarla con los datos de talleres
+
+const formArreglos = document.getElementById('form-arreglos');
+const resultadosArreglos = document.getElementById('resultado-arreglo');
+const selectOperacionArreglo = document.getElementById('operacion-arreglo');
+
+formArreglos.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+    const operacion = selectOperacionArreglo.value;
+    let resultado;
+    switch(operacion){
+        case 'ForEach':
+            resultado = talleres.map((t) => `- ${t.nombre} (${t.inscritos}/${t.cupo})`).join(` \n`);
+            break;
+    }
+
+    resultadosArreglos.textContent = resultado;
+})
